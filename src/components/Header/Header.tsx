75930e5907d89f58b4fd91bc113cbbe4885b881a
Thiago Firmiano <thiaguinho.pentas@gmail.com>
2023-01-29 23:18:21 -0300

    feat: Ajeitando Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ export const Header: React.FC = () => (
   <MHeader>
     <Bar>
       <Link><a href="#competências">Competências</a></Link>
-      <Link><a href="#portfolio">Local onde divulgo minhas experiências</a></Link>
+      <Link><a href="#portfolio">Divulgo experiências</a></Link>
       <Link><a href="#contato">Contatos</a></Link>
     </Bar>
   </MHeader>
